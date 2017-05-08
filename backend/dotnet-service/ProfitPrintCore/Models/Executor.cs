037a7065327c619f726f148a61cfba90d372e0df
DarienTLOD <darienyry@gmail.com>
2017-05-08 14:31:08 +0300

    Added necessary references.

---
+++ backend/dotnet-service/ProfitPrintCore/Models/Executor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProfitPrintCore.Models
+{
+    public partial class Executor
+    {
+        public long Id { get; set; }
+
+        public virtual User IdNavigation { get; set; }
+    }
+}
