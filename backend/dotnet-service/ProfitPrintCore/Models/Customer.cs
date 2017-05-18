dfe41cbc11a673c3161f17b64f4af5a794884339
DarienTLOD <darienyry@gmail.com>
2017-05-18 19:08:09 +0300

    Added new models.

--- backend/dotnet-service/ProfitPrintCore/Models/Customer.cs
+++ backend/dotnet-service/ProfitPrintCore/Models/Customer.cs
@@ -5,10 +5,16 @@ namespace ProfitPrintCore.Models
 {
     public partial class Customer
     {
+        public Customer()
+        {
+            Order = new HashSet<Order>();
+        }
+
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
 
+        public virtual ICollection<Order> Order { get; set; }
         public virtual User IdNavigation { get; set; }
     }
 }
--- backend/dotnet-service/ProfitPrintCore/Models/Executor.cs
+++ backend/dotnet-service/ProfitPrintCore/Models/Executor.cs
@@ -5,14 +5,23 @@ namespace ProfitPrintCore.Models
 {
     public partial class Executor
     {
+        public Executor()
+        {
+            Executorserviceitem = new HashSet<Executorserviceitem>();
+            Order = new HashSet<Order>();
+        }
+
         public long Id { get; set; }
         public string Address { get; set; }
         public TimeSpan CloseTime { get; set; }
         public double Latitude { get; set; }
         public double Longtitude { get; set; }
         public TimeSpan OpenTime { get; set; }
+        public string Title { get; set; }
         public string WebSite { get; set; }
 
+        public virtual ICollection<Executorserviceitem> Executorserviceitem { get; set; }
+        public virtual ICollection<Order> Order { get; set; }
         public virtual Registerkey Registerkey { get; set; }
         public virtual User IdNavigation { get; set; }
     }
