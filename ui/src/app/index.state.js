0c8a437d858c6b443707f73f9f8ca57fce313c46
bitreight <alexey.kur@gmail.com>
2017-05-23 00:45:51 +0300

    Refactored routes.

--- ui/src/app/index.state.js
+++ ui/src/app/index.state.js
@@ -7,26 +7,29 @@
 
   function routeConfig($stateProvider, $urlRouterProvider) {
     $stateProvider
+      //customer routes
       .state('customerMain', {
         url: '/',
         templateUrl: 'app/customer/main/main.html',
         controller: 'MainController',
         controllerAs: 'mainCtrl'
       })
+
+      //executor routes
       .state('executorMain', {
         url: '/crm',
-        templateUrl: 'app/executor/root_crm.html',
+        templateUrl: 'app/executor/rootCrm.html',
         abstract: true
       })
       .state('executorLogin', {
         parent: 'executorMain',
         url: '/login',
-        templateUrl: 'app/executor/login/login.html'
+        templateUrl: 'app/executor/login/loginPage.html'
       })
       .state('executorRegister', {
         parent: 'executorMain',
         url: '/register',
-        templateUrl: 'app/executor/register/register.html'
+        templateUrl: 'app/executor/register/registerPage.html'
       });
 
     $urlRouterProvider.otherwise('/');
